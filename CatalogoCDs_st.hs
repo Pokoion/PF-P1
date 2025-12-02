3a4fc286c789bcf94efcc821b1798fc9c86e7397
Ibai <ibaionatibia@gmail.com>
2025-12-02 11:34:04 +0100

    CargarCatalogosDesdeFichero

--- CatalogoCDs_st.hs
+++ CatalogoCDs_st.hs
@@ -1,3 +1,4 @@
+
 -------------------------------------------------------------------------------
 --  PRACTICA: Gestion de Catalogo de Contenido Digital     
 --  PF  2025-2026
@@ -10,6 +11,8 @@
 -------------------------------------------------------------------------------
 module CatalogoCD where
 -- import Data.XXX
+
+-- Implementacion propia de splitOn
 type Titulo = String
 type Serie = (Titulo, NTemporadas, EpisodiosXTemporada, DuracionM, GeneroS, Edad )
 data GeneroS = Accion | Animacion | Comedia | Drama | Documental | SciFic | Suspense | Romance | Terror deriving(Eq,Ord,Show)
@@ -139,3 +142,57 @@ getAllGeneroS = [Accion, Animacion, Comedia, Drama, Documental, SciFic, Suspense
 
 duracionTotal :: Serie -> DuracionM
 duracionTotal x = getEpisodiosTotales x * getDuracionEp x 
+
+splitOn :: String -> String -> [String]
+splitOn delim = split'
+  where
+    split' [] = [""]
+    split' s = 
+      case startsWith delim s of
+        Just rest -> "" : split' rest
+        Nothing -> let (c:cs) = s
+                       (x:xs) = split' cs 
+                   in (c:x):xs
+    
+    startsWith [] s = Just s
+    startsWith _ [] = Nothing
+    startsWith (d:ds) (c:cs)
+      | d == c = startsWith ds cs
+      | otherwise = Nothing
+
+parseGeneroS:: String -> GeneroS
+parseGeneroS generoS =
+    case generoS of 
+        "Accion"-> Accion
+        "Animacion"->Animacion
+        "Comedia"->Comedia
+        "Drama" -> Drama
+        "Documental" -> Documental
+        "SciFic" -> SciFic
+        "Suspense" -> Suspense
+        "Romance" -> Romance
+        "Terror" -> Terror
+
+convertirLineaEnSerie :: String -> Serie
+convertirLineaEnSerie linea = 
+    let [titulo, temp, epis, dur, gen, ed] = splitOn ";" linea
+        ntemporadas = read temp :: Int
+        episodios = read epis :: Int
+        duracion = read dur :: Int
+        genero = parseGeneroS gen
+        edad = read ed :: Int
+    in (titulo, ntemporadas, episodios, duracion, genero, edad)
+
+-- ============================
+-- Funciones de la segunda entrega
+-- ============================
+
+--Cargar catalogos desde un fichero 
+cargarCatalogosDesdeFichero:: FilePath -> IO [Serie]
+cargarCatalogosDesdeFichero filepath = do 
+                                      contenido <- readFile filepath
+                                      let lineasTexto = lines contenido
+                                      let series = map convertirLineaEnSerie lineasTexto
+                                      return series
+
+
